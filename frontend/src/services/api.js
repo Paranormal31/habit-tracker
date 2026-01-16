62012e77ef0401be5e69e627deddf2066ceaf650
Paranormal31 <sarthakn02317@gmail.com>
2026-01-16 16:43:02 +0530

    Update API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://habit-tracker-backend-qgvy.onrender.com/api",
 });
 
 export default API;
